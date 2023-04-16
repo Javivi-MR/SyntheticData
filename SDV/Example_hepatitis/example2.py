70244f7a3b3e7a034eb7dae9f5aad54124643f93
Francisco Javier Molina Rojas <99264371+Javivi-MR@users.noreply.github.com>
2023-04-16 02:43:57 +0200

    Commented

--- SDV/Example_hepatitis/example2.py
+++ SDV/Example_hepatitis/example2.py
@@ -1,9 +1,9 @@
 import pandas as pd # Libreria para la manipulacion de datos
 from sklearn.datasets import load_iris # Usaremos el dataset iris
-from sdv.single_table import CTGANSynthesizer
-from sdv.metadata import SingleTableMetadata
-from sdv.evaluation.single_table import evaluate_quality
-from sdv.evaluation.single_table import get_column_plot
+from sdv.single_table import CTGANSynthesizer # Importamos el sintetizador
+from sdv.metadata import SingleTableMetadata # Importamos el modulo para crear la tabla de metadatos
+from sdv.evaluation.single_table import evaluate_quality # importamos el modulo para la evaluacion de los datos
+from sdv.evaluation.single_table import get_column_plot 
 import matplotlib.pyplot as plt # Libreria para poder realizar la regresionç
 import numpy as np
 
@@ -12,34 +12,33 @@ try:
 	#we will use pandas library to achieve this
 	Columns = ["Clase","Edad","Sexo","Tratamiento con esteroides","Tratamiento antiviral","Fatiga","Malestar general","Anorexia","Hepatomegalia","Firmeza hepática","Esplenomegalia palpable","Arañas vasculares","Ascitis","Varices esofágicas","Bilirrubina sérica total","Fosfatasa alcalina sérica","Transaminasa glutámico-oxalacética sérica","Albúmina sérica","Tiempo de protrombina","Histología"]
     
+    #Creamos un data frame con los datos originales
 	original_data_frame = pd.read_csv("hepatitis.data",names=Columns)
-	#now we can start working
+	#Creamos la tabla de metadatos
 	original_metadata = SingleTableMetadata()
 	original_metadata.detect_from_dataframe(data=original_data_frame)
-	
+	#Configuramos el modelo, en este caso usamos CTGAN
 	synthesizer = CTGANSynthesizer(original_metadata, enforce_rounding=False,epochs=500,verbose=True)
-
+    #Ponemos el modelo a entrenar
 	synthesizer.fit(original_data_frame)
-	
+	#Una vez entrenado, cogemos una muestra de 155 filas
 	synthetic_data = synthesizer.sample(num_rows=155)
-
-	original_data_frame.replace('?', np.nan, inplace=True)
-	synthetic_data.replace('?', np.nan, inplace=True)
 
 	print(synthetic_data)
 
+    #Creamos el informe de evalucion
 	quality_report = evaluate_quality(
     real_data=original_data_frame,
     synthetic_data=synthetic_data,
     metadata=original_metadata)
 
+    #Creamos el grafico de comparacion
 	fig = get_column_plot(
     real_data=original_data_frame,
     synthetic_data=synthetic_data,
     column_name='Albúmina sérica',
-    metadata=original_metadata
-	)
-    
+    metadata=original_metadata)
+    #mostramos la figura
 	fig.show()
 
 except FileNotFoundError:
